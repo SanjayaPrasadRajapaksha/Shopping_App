bd0ff70148ab20ad5cbc0e93b2fe76c887e3b0d8
vihara-senindu <viharasenindu22@gmail.com>
2025-01-23 15:13:28 +0530

    App, Relatedproduct, ShopContext, Product Updated

--- frontend/src/pages/Product.jsx
+++ frontend/src/pages/Product.jsx
@@ -2,10 +2,11 @@ import React, { useContext, useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import { ShopContext } from "../context/ShopContext";
 import { assets } from "../assets/assets";
+import RelatedProducts from "../components/RelatedProducts";
 
 const Product = () => {
   const { productID } = useParams();
-  const { products, currency } = useContext(ShopContext);
+  const { products, currency, addToCart } = useContext(ShopContext);
   const [productData, setProductData] = useState(false);
   const [image, setImage] = useState("");
   const [size, setSize] = useState("");
@@ -15,7 +16,6 @@ const Product = () => {
       if (item._id === productID) {
         setProductData(item);
         setImage(item.image[0]);
-        console.log(item);
         return null;
       }
     });
@@ -81,7 +81,7 @@ const Product = () => {
               ))}
             </div>
           </div>
-          <button className="bg-black text-white px-8 py-3 text-sm active:bg-gray-700 ">
+          <button onClick={()=>addToCart(productData._id,size)} className="bg-black text-white px-8 py-3 text-sm active:bg-gray-700 ">
             ADD TO CART
           </button>
           <hr className="mt-8 sm:w-4/5 " />
@@ -117,7 +117,7 @@ const Product = () => {
         </div>
       </div>
       {/* Display related produts */}
-      
+      <RelatedProducts category={productData.category} subCategory={productData.subCategory}/>
     </div>
   ) : (
     <div className="opacity-0"></div>
