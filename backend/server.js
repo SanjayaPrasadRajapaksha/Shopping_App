f7873741c45fbb990f091851b90f7c500f952fdc
Sanjaya Prasad <130231733+SanjayaPrasadRajapaksha@users.noreply.github.com>
2025-02-24 21:26:57 +0530

    Setup Cloudinary configuration

--- backend/server.js
+++ backend/server.js
@@ -2,11 +2,13 @@ import express from 'express';
 import cors from 'cors';
 import 'dotenv/config'
 import connectDB from './config/mongodb.js';
+import connectCloudnary from './config/cloudnary.js';
 
 //App Config
 const app = express();
 const port = process.env.PORT || 4000;
 connectDB()
+connectCloudnary()
 
 //middlewares
 app.use(express.json());
