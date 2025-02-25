b40dd265393edf5a75f61a97e2ab666e61903fd0
Sanjaya Prasad <130231733+SanjayaPrasadRajapaksha@users.noreply.github.com>
2025-02-25 11:19:40 +0530

    Implement user login API with route

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -10,6 +10,28 @@ const createToken = (id) => {
 // Route for user login
 
 const loginUser = async (req, res) => {
+    try {
+        const { email, password } = req.body;
+        const user = await userModel.findOne({ email });
+
+        if (!user) {
+            return res.status(400).json({ success: false, msg: "User doesn't found" });
+        }
+
+        const isMatch = await bcrypt.compare(password, user.password);
+
+        if (isMatch) {
+            const token = createToken(user._id);
+            res.json({ success: true, token });
+        }
+        else {
+            return res.status(400).json({ success: false, msg: "Invalid credentials" });
+        }
+
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ success: false, msg: error.message });
+    }
 
 };
 
@@ -43,7 +65,7 @@ const registerUser = async (req, res) => {
 
         const token = createToken(user._id);
         res.status(201).json({ success: true, token });
-    } catch (error) { 
+    } catch (error) {
         console.error(error);
         res.status(500).json({ success: false, msg: error.message });
     }
