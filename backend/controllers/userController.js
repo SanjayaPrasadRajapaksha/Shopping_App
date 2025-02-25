437ff9d308b1a20ff57b48733b63a4c5831ef82b
Sanjaya Prasad <130231733+SanjayaPrasadRajapaksha@users.noreply.github.com>
2025-02-25 15:25:03 +0530

    Implement product creation function

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -15,7 +15,7 @@ const loginUser = async (req, res) => {
         const user = await userModel.findOne({ email });
 
         if (!user) {
-            return res.status(400).json({ success: false, msg: "User doesn't found" });
+            return res.status(400).json({ success: false, message: "User doesn't found" });
         }
 
         const isMatch = await bcrypt.compare(password, user.password);
@@ -25,12 +25,12 @@ const loginUser = async (req, res) => {
             res.json({ success: true, token });
         }
         else {
-            return res.status(400).json({ success: false, msg: "Invalid credentials" });
+            return res.status(400).json({ success: false, message: "Invalid credentials" });
         }
 
     } catch (error) {
         console.error(error);
-        res.status(500).json({ success: false, msg: error.message });
+        res.status(500).json({ success: false, message: error.message });
     }
 
 };
@@ -44,15 +44,15 @@ const registerUser = async (req, res) => {
         //check user already exists or not
         const exists = await userModel.findOne({ email });
         if (exists) {
-            return res.status(400).json({ success: false, msg: "User already exists" });
+            return res.status(400).json({ success: false, message: "User already exists" });
         }
 
         // validating email format and strong password
         if (!validator.isEmail(email)) {
-            return res.status(400).json({ success: false, msg: "Please enter a valid email" });
+            return res.status(400).json({ success: false, message: "Please enter a valid email" });
         }
         if (!validator.isLength(password, { min: 8 })) {
-            return res.status(400).json({ success: false, msg: "Password should be at least 8 characters long" });
+            return res.status(400).json({ success: false, message: "Password should be at least 8 characters long" });
         }
 
         // hashing user password
@@ -67,7 +67,7 @@ const registerUser = async (req, res) => {
         res.status(201).json({ success: true, token });
     } catch (error) {
         console.error(error);
-        res.status(500).json({ success: false, msg: error.message });
+        res.status(500).json({ success: false, message: error.message });
     }
 };
 
