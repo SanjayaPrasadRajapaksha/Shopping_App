f2cd9666f5303061f481d8b00013a81dd7536176
Sanjaya Prasad <130231733+SanjayaPrasadRajapaksha@users.noreply.github.com>
2025-02-28 22:34:55 +0530

    Handle user logout by clearing authentication token

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -5,13 +5,20 @@ import { ShopContext } from "../context/ShopContext";
 
 const Navbar = () => {
   const [visible, setVisible] = useState(false);
-  const { setShowSearch, getCartCount } = useContext(ShopContext);
+  const { setShowSearch, getCartCount, navigate, token, setToken, setCartItems } = useContext(ShopContext);
 
+  const logout = () => {
+    navigate('/login');
+    localStorage.removeItem('token');
+    setToken('');
+    setCartItems({});
+
+  }
   return (
     <div className="flex items-center justify-between py-5 font-medium">
-      <Link to="/">
+     
         <img src={assets.logo} className="w-36" alt="logo" />
-      </Link>
+    
       <ul className="hidden sm:flex gap-5 text-sm text-gray-700">
         <NavLink to="/" className="flex flex-col items-center gap-1">
           <p>HOME</p>
@@ -49,7 +56,7 @@ const Navbar = () => {
             <div className="flex flex-col gap-2 w-36 py-3 px-5 bg-slate-100 text-gray-500 rounded">
               <p className="cursor-pointer hover:text-black ">My Profile</p>
               <p className="cursor-pointer hover:text-black ">Orders</p>
-              <p className="cursor-pointer hover:text-black ">Logout</p>
+              <p onClick={logout} className="cursor-pointer hover:text-black ">Logout</p>
             </div>
           </div>
         </div>
@@ -70,9 +77,8 @@ const Navbar = () => {
       {/* SideBar menu for small screens*/}
 
       <div
-        className={`absolute top-0 right-0 bottom-0 overflow-hidden bg-white transition-all ${
-          visible ? "w-full" : "w-0"
-        } `}
+        className={`absolute top-0 right-0 bottom-0 overflow-hidden bg-white transition-all ${visible ? "w-full" : "w-0"
+          } `}
       >
         <div className="flex flex-col text-gray-600">
           <div
