ef9b930630ed2765aacaa733321a1db6e164f88f
Sanjaya Prasad <130231733+SanjayaPrasadRajapaksha@users.noreply.github.com>
2025-03-02 21:35:48 +0530

    Implement addToCart logic

--- backend/controllers/cartController.js
+++ backend/controllers/cartController.js
@@ -1,7 +1,29 @@
+import userModel from "../models/userModel.js";
+
 // add product to user cart
 const addToCart = async (req, res) => {
+    try {
+        const { userId, itemId, size } = req.body;
+        const userData = await userData.cartData;
 
+        if (cartData[itemId]) {
+            if (cartData[itemId][[size]]) {
+                cartData[itemId][size] += 1;
+            } else {
+                cartData[itemId][size] = 1;
+            }
+        } else {
+            cartData[itemId] = {};
+            cartData[itemId][size] = 1;
+        }
+        await userModel.findByIdAndUpdate(itemId, { cartData });
+        res.json({ success: true, message: "Added to cart" });
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ success: false, message: error.message });
+    }
 }
+
 
 // update user cart
 const updateCart = async (req, res) => {
