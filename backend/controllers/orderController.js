25d187c090b7c26e9187eac8850d6ea4dfb2c281
Sanjaya Prasad <130231733+SanjayaPrasadRajapaksha@users.noreply.github.com>
2025-03-04 21:02:11 +0530

    Implement API endpoint for  managing orders

---
+++ backend/controllers/orderController.js
@@ -0,0 +1,38 @@
+// Placing order using COD method
+const placeOrder = async (req, res) => {
+
+}
+
+// Placing order using stripe method
+const placeOrderStripe = async (req, res) => {
+
+}
+
+// Placing order using razorpay method
+const placeOrderRazorpay = async (req, res) => {
+
+}
+
+// All orders data for Admin panel
+const allOrders = async (req, res) => {
+
+}
+
+// User order data for Frontend
+const userOrders = async (req, res) => {
+
+}
+
+// User order status for Admin panel
+const updateStatus = async (req, res) => {
+
+}
+
+export {
+    placeOrder,
+    placeOrderStripe,
+    placeOrderRazorpay,
+    allOrders,
+    userOrders,
+    updateStatus,
+}
