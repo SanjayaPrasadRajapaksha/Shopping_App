bed839c91bcdccbf39524eaf05e65f5011bfbe95
Sanjaya Prasad <130231733+SanjayaPrasadRajapaksha@users.noreply.github.com>
2025-03-06 13:53:33 +0530

    Change footer details

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -23,14 +23,14 @@ const Footer = () => {
         <div>
           <p className="text-xl font-medium mb-5">GET IN TOUCH</p>
           <ul className="flex flex-col gap-1 text-gray-600">
-            <li>+1-212-456-7890</li>
-            <li>Contact@foreveryou.com</li>
+            <li>+94-212-456-7890</li>
+            <li>Contact@drassbay.com</li>
           </ul>
         </div>
       </div>
       <div>
         <hr />
-        <p className="py-5 text-sm text-center">Copyright 2024@ forever.com - All Right Reserved.</p>
+        <p className="py-5 text-sm text-center">Copyright 2025@ dressbay.com - All Right Reserved.</p>
       </div>
     </div>
   );
