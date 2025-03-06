d83a12d4869dca35ef97ef0321e206399ed0c6fa
Sanjaya Prasad <130231733+SanjayaPrasadRajapaksha@users.noreply.github.com>
2025-03-06 13:59:44 +0530

    Update Best Seller section text

--- frontend/src/components/BestSeller.jsx
+++ frontend/src/components/BestSeller.jsx
@@ -17,9 +17,7 @@ const BestSeller = () => {
       <div className="text-center text-3xl py-8">
         <Title text1={"BEST"} text2={"SELLER"} />
         <p className="w-3/4 m-auto text-xs sm:text-sm md:text-base text-gray-600">
-          Lorem ipsum dolor sit amet consectetur, adipisicing elit. Officia
-          facere dolor impedit sed velit. Optio mollitia consequuntur debitis
-          sint a.
+        Explore our top-selling products that have won the trust of our customers. From high-quality essentials to popular favorites, these items are in demand for their exceptional value and style!
         </p>
       </div>
       <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 gap-4 gap-y-6">
